94970b352f544690ff8330b44bf0d0e6a7e9ec66
NulogicSystems <james@nulogicsystems.com>
2017-05-01 13:42:41 -0400

    Update Fav Menus

--- usr/share/plasma/look-and-feel/org.kde.netrunner-rolling.desktop/contents/plasmoidsetupscripts/org.kde.plasma.kickoff.js
+++ usr/share/plasma/look-and-feel/org.kde.netrunner-rolling.desktop/contents/plasmoidsetupscripts/org.kde.plasma.kickoff.js
@@ -3,7 +3,7 @@ applet.writeConfig("global", "Alt+F1")
 
 applet.currentConfigGroup = ["General"]
 applet.writeConfig("icon", "file:///usr/share/icons/hicolor/scalable/apps/homerun.svg")
-applet.writeConfig("favorites", ["org.gnome.Software.desktop", "octopi.desktop", "org.kde.ksysguard.desktop", "systemsettings.desktop"])
+applet.writeConfig("favorites", ["systemsettings.desktop", "org.kde.ksysguard.desktop","org.gnome.Software.desktop", "octopi.desktop"])
 applet.writeConfig("alphaSort", true)
 applet.writeConfig("hiddenApplications", "org.kde.ksshaskpass.desktop,libreoffice-draw.desktop,libreoffice-base.desktop,libreoffice-math.desktop,lximage-qt-screenshot.desktop,audacious-qt.desktop,mpv.desktop")
 
