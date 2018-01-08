6dde68fb5da8dc69470f3ff6d8a8d186eb966c0f
nulogicsystems <james@nulogicsystems.com>
2018-01-07 22:08:53 -0500

    Add yarock to shown items

--- usr/share/plasma/look-and-feel/org.kde.netrunner-rolling.desktop/contents/plasmoidsetupscripts/org.kde.plasma.private.systemtray.js
+++ usr/share/plasma/look-and-feel/org.kde.netrunner-rolling.desktop/contents/plasmoidsetupscripts/org.kde.plasma.private.systemtray.js
@@ -2,5 +2,5 @@ applet.currentConfigGroup = ["General"]
 applet.writeConfig("extraItems","org.kde.plasma.devicenotifier,org.kde.plasma.networkmanagement,org.kde.discovernotifier,org.kde.plasma.diskquota,org.kde.plasma.bluetooth,org.kde.plasma.clipboard,org.kde.plasma.printmanager,org.kde.plasma.battery")
 applet.writeConfig("knownItems", "org.kde.plasma.volume,org.kde.plasma.networkmanagement,org.kde.plasma.bluetooth,org.kde.plasma.battery,org.kde.discovernotifier,org.kde.plasma.clipboard,org.kde.plasma.mediacontroller,org.kde.plasma.devicenotifier,org.kde.plasma.notifications,org.kde.plasma.printmanager,org.kde.plasma.notifications,org.kde.kdeconnect")
 applet.writeConfig("hiddenItems","org.kde.plasma.clipboard")
-applet.writeConfig("shownItems", "transmission,vokoscreen")
+applet.writeConfig("shownItems", "transmission,vokoscreen,yarock")
 applet.reloadConfig()
