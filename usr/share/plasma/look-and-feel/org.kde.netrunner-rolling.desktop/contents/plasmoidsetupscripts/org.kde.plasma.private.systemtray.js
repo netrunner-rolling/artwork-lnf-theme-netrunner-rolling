0e67857c2e1a5126fd85f07d70a75df1c571a637
nulogicsystems <james@nulogicsystems.com>
2018-06-19 00:32:32 -0400

    Modifications
    Hide volumewin7mixer from systemtray
    Remove duplicate entry for notifications

--- usr/share/plasma/look-and-feel/org.kde.netrunner-rolling.desktop/contents/plasmoidsetupscripts/org.kde.plasma.private.systemtray.js
+++ usr/share/plasma/look-and-feel/org.kde.netrunner-rolling.desktop/contents/plasmoidsetupscripts/org.kde.plasma.private.systemtray.js
@@ -1,6 +1,6 @@
 applet.currentConfigGroup = ["General"]
 applet.writeConfig("extraItems","org.kde.plasma.devicenotifier,org.kde.plasma.networkmanagement,org.kde.discovernotifier,org.kde.plasma.diskquota,org.kde.plasma.bluetooth,org.kde.plasma.clipboard,org.kde.plasma.printmanager,org.kde.plasma.battery")
-applet.writeConfig("knownItems", "org.kde.plasma.volume,org.kde.plasma.networkmanagement,org.kde.plasma.bluetooth,org.kde.plasma.battery,org.kde.discovernotifier,org.kde.plasma.clipboard,org.kde.plasma.mediacontroller,org.kde.plasma.devicenotifier,org.kde.plasma.notifications,org.kde.plasma.printmanager,org.kde.plasma.notifications,org.kde.kdeconnect")
-applet.writeConfig("hiddenItems","org.kde.plasma.clipboard")
+applet.writeConfig("knownItems", "org.kde.plasma.volume,org.kde.plasma.networkmanagement,org.kde.plasma.bluetooth,org.kde.plasma.battery,org.kde.discovernotifier,org.kde.plasma.clipboard,org.kde.plasma.mediacontroller,org.kde.plasma.devicenotifier,org.kde.plasma.notifications,org.kde.plasma.printmanager,org.kde.kdeconnect,org.kde.plasma.volumewin7mixer")
+applet.writeConfig("hiddenItems","org.kde.plasma.clipboard,org.kde.plasma.volumewin7mixer")
 applet.writeConfig("shownItems", "transmission,vokoscreen,yarock")
 applet.reloadConfig()
