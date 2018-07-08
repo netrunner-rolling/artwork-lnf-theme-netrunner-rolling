e1730c68a3fe24b8942b15aa0dfe7a1c53b59dcf
nulogicsystems <james@nulogicsystems.com>
2018-07-08 01:55:57 -0400

    Modifications
    Change default wallpaper for 2018.06
    Update preview

--- usr/share/plasma/look-and-feel/org.kde.netrunner-rolling.desktop/contents/plasmoidsetupscripts/org.kde.plasma.folder.js
+++ usr/share/plasma/look-and-feel/org.kde.netrunner-rolling.desktop/contents/plasmoidsetupscripts/org.kde.plasma.folder.js
@@ -1,6 +1,6 @@
 applet.wallpaperPlugin = 'org.kde.image'
 applet.currentConfigGroup = ["Wallpaper", "org.kde.image", "General"]
-applet.writeConfig("Image", "file:///usr/share/wallpapers/netrunner-rolling/Netrunner Rolling.png")
+applet.writeConfig("Image", "file:///usr/share/wallpapers/netrunner-rolling-201806/Rolling 2018.06.jpg")
 applet.writeConfig("FillMode", "2")
 applet.currentConfigGroup = new Array("General");
 applet.writeConfig("positions","1,7,desktop:/mycomputer.desktop,0,0,desktop:/Network,0,1,desktop:/welcome.desktop,0,2,desktop:/calamares.desktop,0,3")
