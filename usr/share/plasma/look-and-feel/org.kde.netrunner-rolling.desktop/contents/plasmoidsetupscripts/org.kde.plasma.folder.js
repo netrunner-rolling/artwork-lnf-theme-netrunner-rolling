f19f3b50824e34ed5c90795121b8286d86275644
nulogicsystems <james@nulogicsystems.com>
2019-03-28 11:40:12 -0400

    update wallpaper

--- usr/share/plasma/look-and-feel/org.kde.netrunner-rolling.desktop/contents/plasmoidsetupscripts/org.kde.plasma.folder.js
+++ usr/share/plasma/look-and-feel/org.kde.netrunner-rolling.desktop/contents/plasmoidsetupscripts/org.kde.plasma.folder.js
@@ -1,6 +1,6 @@
 applet.wallpaperPlugin = 'org.kde.image'
 applet.currentConfigGroup = ["Wallpaper", "org.kde.image", "General"]
-applet.writeConfig("Image", "file:///usr/share/wallpapers/netrunner-rolling-201808/Netrunner Rolling 2018.08.jpg")
+applet.writeConfig("Image", "file:///usr/share/wallpapers/netrunner-rolling/Dark_Rolling_Wallpaper_.jpg")
 applet.writeConfig("FillMode", "2")
 applet.currentConfigGroup = new Array("General");
 applet.writeConfig("positions","1,7,desktop:/mycomputer.desktop,0,0,desktop:/Network,0,1,desktop:/welcome.desktop,0,2,desktop:/calamares.desktop,0,3")
